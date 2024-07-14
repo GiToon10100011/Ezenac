781e7a7ad5ef44fc07945077acd656148389aa46
GiToon10100011 <boon10034@gmail.com>
2024-07-14 12:00:25 +0900

    --Full Page Slider Completed--

--- Full_Page_Slider/script.js
+++ Full_Page_Slider/script.js
@@ -1,9 +1,29 @@
 const labels = document.querySelectorAll(".btn span");
 const container = document.querySelector(".items");
+const titles = container.querySelectorAll("h1");
+const btns = container.querySelectorAll("button");
+
+titles[0].classList.add("content");
+btns[0].classList.add("content");
 
 labels.forEach((items, index) => {
   items.addEventListener("click" , () => {
     items.classList.add("active");
+    
+    titles.forEach((title, i) => {
+      title.classList.add("content");
+      if(index !== i){
+        title.classList.remove("content");
+      }
+    });
+
+    btns.forEach((btn, i) => {
+      btn.classList.add("content");
+      if(index !== i){
+        btn.classList.remove("content");
+      }
+    });
+    
     container.style.left = `-${100*index}%`;
 
     labels.forEach((label, i) => { 
