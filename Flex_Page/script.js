dea21c70b7596457f765e163bb0900f202137320
GiToon10100011 <boon10034@gmail.com>
2024-07-21 02:18:53 +0900

    0721 Update

--- Flex_Page/script.js
+++ Flex_Page/script.js
@@ -1,14 +1,62 @@
+
 // IntroVid
-// const intro = document.querySelector(".intro_video");
+const intro = document.querySelector(".intro_video");
 
-// const introVid = intro.querySelector("video");
+const introVid = intro.querySelector("video");
 
-// introVid.addEventListener("ended", () => {
-//   intro.classList.add("active");
-//   setTimeout(() => {
-//     intro.style.display = "none";
-//   }, 600)
-// })
+introVid.addEventListener("ended", () => {
+    intro.classList.add("active");
+    setTimeout(() => {
+        intro.style.display = "none";
+  }, 600)
+})
+
+// Changing Modes
+const all = document.querySelectorAll("*");
+
+const lnb = document.querySelector(".lnb");
+const modes = lnb.querySelectorAll("li");
+
+let initialCount = 0;
+
+const vids = ["Intro1.mp4", "Intro2.mp4", "Intro3.mp4"];
+
+const logo = document.querySelector(".logo a img");
+const logos = ["Logo1.png", "Logo2.png", "Logo3.png"];
+const colors = ["#e60012", "#030098", "#0f7c0f"];
+
+let colorStyle = [];
+
+
+all.forEach((el) => {
+  const colorS = getComputedStyle(el);
+  if (colorS.color === colors[0]) {
+    colorStyle.push(el);
+  } 
+});
+
+console.log(colorStyle);
+
+modes.forEach((mode, index) => {
+  mode.addEventListener("click", () => {
+    introVid.setAttribute("src", `./images/${vids[index]}`);
+    introVid.load();
+    intro.style.display = "block";
+    intro.classList.remove("active");
+    introVid.play();
+
+    logo.setAttribute("src", `./images/${logos[index]}`);
+
+    all.forEach((element) => {
+      const computedStyle = getComputedStyle(element);
+      if (computedStyle.color !== colors[index]) {
+        colorStyle.forEach((c) => {
+          c.style.color = colors[index];
+        })
+      }
+    });
+  });
+});
 
 // Header
 
@@ -34,16 +82,16 @@ Top.addEventListener("click", () => {
 
 // SlickSlider
 $(".myslider").slick({
-  arrows : true,
-  dots : false,
+  arrows: true,
+  dots: false,
   infinite: true,
   speed: 300,
   slidesToShow: 3,
   slidesToScroll: 3,
   autoplay: true,
   autoplaySpeed: 3000,
-  adaptiveHeight : true,
-  pauseOnHover : true,
+  adaptiveHeight: true,
+  pauseOnHover: true,
   responsive: [
     {
       breakpoint: 1024,
@@ -89,7 +137,6 @@ gnbA.forEach((a) => {
     gnb.classList.remove("active");
   });
 });
-
 
 $(".Top, .gnb a").click(function () {
   $.scrollTo(this.hash || 0, 600);
